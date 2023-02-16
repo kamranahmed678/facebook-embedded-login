6c7dcede47d9e398fd5471e25d845ca012345510
kamranahmed678 <104222795+kamranahmed678@users.noreply.github.com>
2023-02-16 10:17:10 +0500

    Embedded signup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
 
 function App() {
+
+  useEffect(() => {
+    // Load the JavaScript SDK asynchronously
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: '719081352898093', // Facebook App ID
+        cookie: true, // enable cookies
+        xfbml: true, // parse social plugins on this page
+        version: 'v16.0', // Graph API version
+      });
+    };
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    // Cleanup function to remove the global variable added by the SDK
+    return function cleanup() {
+      delete window.fbAsyncInit;
+      delete window.FB;
+    };
+  }, []);
+
+  // Facebook Login with JavaScript SDK
+  function launchWhatsAppSignup() {
+    // Conversion tracking code
+    
+    // Check the login status and launch the login dialog if needed
+    window.FB.getLoginStatus(function (response) {
+      if (response.status === 'connected') {
+        const accessToken = response.authResponse.accessToken;
+        console.log(accessToken)
+        //Use this token to call the debug_token API and get the shared WABA's ID
+      } else {
+        window.FB.login(function (response) {
+          if (response.authResponse) {
+            const accessToken = response.authResponse.accessToken;
+            console.log(accessToken)
+            //Use this token to call the debug_token API and get the shared WABA's ID
+          } else {
+            console.log('User cancelled login or did not fully authorize.');
+          }
+        }, {
+          scope: 'business_management,whatsapp_business_management',
+          extras: {
+            feature: 'whatsapp_embedded_signup'
+          }
+        });
+      }
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button
+      onClick={launchWhatsAppSignup}
+      style={{
+        backgroundColor: '#1877f2',
+        border: '0',
+        borderRadius: '4px',
+        color: '#fff',
+        cursor: 'pointer',
+        fontFamily: 'Helvetica, Arial, sans-serif',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        height: '40px',
+        padding: '0 24px'
+      }}
+    >
+      Login with Facebook
+    </button>
   );
 }
 
